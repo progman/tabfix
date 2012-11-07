7d0f4150759fe6644098c2dc640189d30d1df169
progman <gnuplanet@gmail.com>
2012-11-07 15:21:04 +0400

    add read from stdin

--- tabfix.cpp
+++ tabfix.cpp
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-// 0.0.2
+// 0.0.3
 // Alexey Potehin http://www.gnuplanet.ru/doc/cv
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 #include <stdio.h>
@@ -9,30 +9,21 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
+#include <string>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <algorithm>
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // global vars
 namespace global
 {
 	bool flag_kill_backup = false;
+	bool flag_debug       = false;
 }
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // convert space to tab
-int tabfix(const char *filename, const char* p, size_t size)
+int tabfix(FILE *fh, const char* p, size_t size)
 {
-	int rc; // error code
-
-
-// open new file
-	FILE *fh = fopen(filename, "a+");
-	if (fh == NULL)
-	{
-		printf ("ERROR[fopen()]: %s\n", strerror(errno));
-		return -1;
-	}
-
-
 	size_t offset = 0;
 	bool flag_head_char = true;
 	bool flag_head_line = true;
@@ -93,25 +84,37 @@ int tabfix(const char *filename, const char* p, size_t size)
 	}
 
 
-// flush file
-	rc = fflush(fh);
-	if (rc != 0)
-	{
-		printf ("ERROR[fflush()]: %s\n", strerror(errno));
-		return -1;
-	}
-
-
-// close file
-	rc = fclose(fh);
-	if (rc != 0)
-	{
-		printf ("ERROR[fclose()]: %s\n", strerror(errno));
-		return -1;
-	}
-
-
 	return 0;
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+// convert string to boolean
+bool str2bool(const std::string& str)
+{
+	std::string tmp = str;
+
+	std::transform(tmp.begin(), tmp.end(), tmp.begin(), tolower);
+
+	if (tmp == "true")
+	{
+		return true;
+	}
+
+	if (tmp == "t")
+	{
+		return true;
+	}
+
+	if (tmp == "on")
+	{
+		return true;
+	}
+
+	if (tmp == "1")
+	{
+		return true;
+	}
+
+	return false;
 }
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // return (concat str1 and str2) or NULL
@@ -134,7 +137,7 @@ char* concat_str(const char* str1, const char* str2)
 }
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // для файла имя которого переданно преобразуем пробелы в таблуляции
-int do_it(const char *filename)
+int do_file(const char *filename)
 {
 	int rc; // error code
 
@@ -193,13 +196,50 @@ int do_it(const char *filename)
 	}
 
 
+// open new file
+	FILE *fh = fopen(filename, "a+");
+	if (fh == NULL)
+	{
+		close(fd);
+		free(backup_filename);
+		printf ("ERROR[fopen()]: %s\n", strerror(errno));
+		return -1;
+	}
+
+
 // convert space to tab
-	rc = tabfix(filename, (const char*)pmmap, size);
+	rc = tabfix(fh, (const char*)pmmap, size);
 	if (rc == -1)
+	{
+		fclose(fh);
+		munmap(pmmap, size);
+		close(fd);
+		free(backup_filename);
+		return -1;
+	}
+
+
+// flush file
+	rc = fflush(fh);
+	if (rc != 0)
+	{
+		fclose(fh);
+		munmap(pmmap, size);
+		close(fd);
+		free(backup_filename);
+		printf ("ERROR[fflush()]: %s\n", strerror(errno));
+		return -1;
+	}
+
+
+// close file
+	rc = fclose(fh);
+	if (rc != 0)
 	{
 		munmap(pmmap, size);
 		close(fd);
 		free(backup_filename);
+		printf ("ERROR[fclose()]: %s\n", strerror(errno));
 		return -1;
 	}
 
@@ -245,23 +285,168 @@ int do_it(const char *filename)
 	return 0;
 }
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-// general function
-int main(int argc, char* argv[])
+// в потоке который подан нам на stdinput преобразуем пробелы в таблуляции
+int do_stdinput()
 {
-	if (argc != 2)
+	int rc;
+	int c;
+
+
+	if (global::flag_debug != false)
 	{
-		printf ("%s\t(%s)\n", PROG_FULL_NAME, PROG_URL);
-		printf ("example: %s source_file\n", PROG_NAME);
-		return 1;
+		printf ("read from stdin...\n");
 	}
 
 
-	if (do_it(argv[1]) == -1)
+	c = getchar();
+	if (c == EOF)
 	{
-		return 1;
+		printf ("\nERROR: stdin is close\n");
+		return -1;
 	}
+
+
+	size_t size   = 4096;
+	size_t delta  = 4096;
+	size_t offset = 0;
+
+
+	void *p_original = malloc(size);
+	if (p_original == NULL)
+	{
+		printf ("ERROR[malloc()]: %s\n", strerror(errno));
+		return -1;
+	}
+	char *p = (char*)p_original;
+
+
+	for(;;)
+	{
+		if (offset >= size)
+		{
+			size += delta;
+			p_original = realloc(p_original, size);
+			if (p_original == NULL)
+			{
+				printf ("ERROR[realloc()]: %s\n", strerror(errno));
+				return -1;
+			}
+			p = (char*)p_original + offset;
+			continue;
+		}
+
+		*p = c; p++; offset++;
+
+		c = getchar();
+		if (c == EOF) break;
+	}
+
+
+// convert space to tab
+	rc = tabfix(stdout, (const char*)p_original, offset);
+	if (rc == -1)
+	{
+		free(p_original);
+		return -1;
+	}
+
+
+// flush stdout
+	fflush(stdout);
+
+
+// free memory
+	free(p_original);
 
 
 	return 0;
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+// view help
+void help()
+{
+	printf ("%s\t(%s)\n", PROG_FULL_NAME, PROG_URL);
+	printf ("example: %s source_file\n", PROG_NAME);
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+// general function
+int main(int argc, char* argv[])
+{
+	int rc;
+	bool flag_file = false;
+
+
+	for(int i=1; i < argc; i++)
+	{
+		std::string key = argv[i];
+		std::string tmpl;
+		std::string value;
+
+
+		if
+		(
+			(key == "-h")    ||
+			(key == "-help") ||
+			(key == "--help")
+		)
+		{
+			help();
+			return 1;
+		}
+
+
+		if (key == "-kb")
+		{
+			global::flag_kill_backup = true;
+			continue;
+		}
+
+
+		tmpl = "--flag_debug=";
+		if ((key.size() >= tmpl.size()) && (key.substr(0, tmpl.size()) == tmpl))
+		{
+			value = key.substr(tmpl.size(), key.size() - 1);
+			global::flag_debug = str2bool(value);
+			continue;
+		}
+
+
+		if (key == "-d")
+		{
+			global::flag_debug = true;
+			continue;
+		}
+
+
+		if
+		(
+			(key == "-")  ||
+			(key == "--")
+		)
+		{
+			break;
+		}
+
+
+		flag_file = true;
+		rc = do_file(key.c_str());
+		if (rc != 0) break;
+	}
+
+
+	if (flag_file == false)
+	{
+		rc = do_stdinput();
+	}
+
+
+	if (rc == 0)
+	{
+//		if (flag_file == true) printf ("Ok.\n");
+		return 0;
+	}
+
+
+	return 1;
 }
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
