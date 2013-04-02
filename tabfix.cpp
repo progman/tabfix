8fdda3a2fa14de7b2f79d6f96fd73f02e4c5cdb8
progman <gnuplanet@gmail.com>
2013-04-02 03:33:20 -0400

    fix realloc behavior

--- tabfix.cpp
+++ tabfix.cpp
@@ -339,6 +339,7 @@ int do_stdinput()
 
 
 	void *p_original = NULL;
+	void *p_original_old;
 	char *p = (char *)p_original;
 
 
@@ -347,9 +348,11 @@ int do_stdinput()
 		if (offset >= size)
 		{
 			size += delta;
+			p_original_old = p_original;
 			p_original = realloc(p_original, size);
 			if (p_original == NULL)
 			{
+				free(p_original_old);
 				printf("ERROR[realloc()]: %s\n", strerror(errno));
 				return -1;
 			}
